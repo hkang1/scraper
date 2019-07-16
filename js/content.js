07b7949c3ac8c27ca5b3facc009bee7a54e49735
hkang1 <hoyoul@gmail.com>
2019-07-15 18:13:20 -0600

    Added download zip support

--- js/content.js
+++ js/content.js
@@ -1,7 +1,6 @@
-const imageUrls = Array.from(document.querySelectorAll('img'))
-  .map(img => img.src);
-
 chrome.runtime.onMessage.addListener((message, sender, response) => {
   if (message.from === 'content') return;
+
+  const imageUrls = Array.from(document.querySelectorAll('img')).map(img => img.src);
   if (message.subject === 'getImageURLs') response(imageUrls);
 });
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,10 @@
 const config = {
-  IMAGE_MIN_SIZE: 150,
-  IMAGE_MAX_HEIGHT: 150
+  IMAGE_MAX_HEIGHT: 100
 };
 
 const data = {
   listeners: [],
-  images: []
+  cells: []
 };
 
 const addListener = (id, type, handler, binding) => {
@@ -21,64 +20,109 @@ const removeAllListeners = () => {
   }
 };
 
+const showEmpty = () => {
+  const nav = document.getElementById('nav');
+  const empty = document.getElementById('empty');
+  nav.style.display = 'none';
+  empty.style.display = 'flex';
+};
+
 const handleContentImageUrls = urls => {
+  const imageUrls = urls || [];
   const gallery = document.getElementById('gallery');
-  (urls || []).forEach(url => {
+  let processed = 0;
+
+  imageUrls.forEach(url => {
     if (url == null || url === '') return;
 
+    const cell = document.createElement('div');
+    const icon = document.createElement('div');
     const img = document.createElement('img');
+    const id = generateId();
+    const info = { id, url, selected: true };
+
+    icon.className = 'icon-checkmark';
+
+    cell.id = id;
+    cell.className = 'cell selected';
+    cell.appendChild(img);
+    cell.appendChild(icon);
 
     img.onload = () => {
-      const id = generateId();
-      const image = { id, selected: true };
-
-      img.id = id;
       img.width = config.IMAGE_MAX_HEIGHT / img.naturalHeight * img.naturalWidth;
       img.height = config.IMAGE_MAX_HEIGHT;
-      img.className = 'selected';
       img.onload = null;
 
-      addListener(img.id, 'click', handleImageToggle, image);
-
-      if ((img.naturalWidth < config.IMAGE_MIN_SIZE ||
-          img.naturalHeight < config.IMAGE_MIN_SIZE) &&
+      if ((img.naturalWidth < config.IMAGE_MAX_HEIGHT ||
+          img.naturalHeight < config.IMAGE_MAX_HEIGHT) &&
           img.parentNode) {
-        img.parentNode.removeChild(img);
+        cell.parentNode.removeChild(cell);
       } else {
-        data.images.push(image);
+        data.cells.push(info);
       }
+
+      processed++;
+
+      if (imageUrls.length === processed && data.cells.length === 0) showEmpty();
     };
 
-    gallery.appendChild(img);
+    gallery.appendChild(cell);
     img.src = url;
+
+    addListener(id, 'click', handleImageToggle, info);
   });
 };
 
 const handleButtonSelectAll = function () {
-  data.images.forEach(image => {
-    const img = document.getElementById(image.id);
-    img.className = 'selected';
-    image.selected = true;
+  data.cells.forEach(info => {
+    const cell = document.getElementById(info.id);
+    cell.className = 'cell selected';
+    cell.selected = true;
   });
 };
 
 const handleButtonDeselectAll = function () {
-  data.images.forEach(image => {
-    const img = document.getElementById(image.id);
-    img.className = '';
-    image.selected = false;
+  data.cells.forEach(info => {
+    const cell = document.getElementById(info.id);
+    cell.className = 'cell';
+    cell.selected = false;
   });
 };
 
 const handleImageToggle = function () {
-  const image = this;
-  const img = document.getElementById(image.id);
-  image.selected = !image.selected;
-  img.className = image.selected ? 'selected' : '';
+  const info = this;
+  const cell = document.getElementById(info.id);
+  cell.selected = !cell.selected;
+  cell.className = cell.selected ? 'cell selected' : 'cell';
 };
 
-window.addEventListener('DOMContentLoaded', () => {
+const handleButtonDownloadZip = function () {
+  const zip = new JSZip();
+
+  data.cells.filter(cell => cell.selected).forEach((cell, index) => {
+    const regex = /^data:image\/(svg\+xml|jpeg|gif|png);base64,(.*)$/i;
+    const matches = cell.url.match(regex);
+    if (matches.length === 3) {
+      const type = matches[1];
+      const filename = generateFilename(index, type);
+      console.log('filename', filename);
+      const content = b64toBlob(matches[2], `image/${type}`);
+      console.log('content', content);
+      zip.file(filename, content);
+    }
+  });
+
+  zip.generateAsync({ type: 'blob' }).then(function (blob) {
+    const filename = 'images.zip';
+    const url = URL.createObjectURL(blob);
+    chrome.downloads.download({ url, filename });
+    // saveAs(blob, filename);
+  });
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
   // Bind listeners to navigation
+  addListener('download-zip', 'click', handleButtonDownloadZip);
   addListener('select-all', 'click', handleButtonSelectAll);
   addListener('deselect-all', 'click', handleButtonDeselectAll);
 
